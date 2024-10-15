02c5597e1b282348ebe9decc695650290060f3f1
emirhan.yildiz <emirhan.yildiz@n11.com>
2024-10-15 13:21:03 +0300

    Refactor chat_with_gpt_endpoint to accept CodeReviewModel as input

--- controllers/GptController.py
+++ controllers/GptController.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Query
 from services.CodeReviewService import review_code_for_clean_principles
+from models.CodeReviewModel import CodeReviewModel
 
 router = APIRouter()
 
 @router.post("/chatWithGpt")
-def chat_with_gpt_endpoint(userInput: str):
-    response = review_code_for_clean_principles(userInput)
+def chat_with_gpt_endpoint(input: CodeReviewModel = Query(...)):
+    response = review_code_for_clean_principles(input)
     return response
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import uvicorn
-from dotenv import load_dotenv
-from openai import OpenAI
-import os
+
 from fastapi import Depends, FastAPI
 from controllers import GptController
 
-load_dotenv()
 
 app = FastAPI()
 
-gpt_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-app.include_router(GptController.router, prefix="/api", dependencies=[Depends(lambda: gpt_client)])
+app.include_router(GptController.router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=5231)
